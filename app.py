7d4bce71ec749dfd008793f386281c005dbca91d
K I S H <175184813+kishxrx@users.noreply.github.com>
2025-09-06 19:56:04 +0000

    added real translation tools

--- app.py
+++ app.py
@@ -32,5 +32,9 @@ def serve(path):
         return send_from_directory(app.static_folder, "index.html")
 
 # ================= RUN SERVER =================
+import os
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
